4fbd6d6ca5fd695d7a435e5dc19d23291a0c7910
lohanxz007 <161086935+lohanxz007@users.noreply.github.com>
2024-10-11 19:11:50 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Enfermagem',
+    'Quanto é o normal de batimento cardíaco?',
+    'A quantidade normal de batimentos é de 50 a 90 batimentos por minuto (bpm)'
+)
+
+criaCartao(
+    'Enfermagem',
+    'Qual é a temperatura que já é febre?',
+    'temperatura maior que 38ºC'
+)
+
+criaCartao(
+    'Enfermagem',
+    'Qual é a definição de anatomia?',
+    'a é a ciência que estuda a forma e a estrutura do organismo humano e das partes que o compõem'
+    
+)
+
+criaCartao(
+    'Enfermagem',
+    'O que é que estuda a embriologia?',
+    'A área da biologia dedicada ao estudo e pesquisa do desenvolvimento embrionário dos seres vivos, desde a fecundação até o nascimento'
+)
